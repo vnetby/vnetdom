7d48ff9b85af051400ec94293b5fc9727e83f200
vnetby <vadzim.kananovich.by@gmail.com>
2020-04-24 18:03:57 +0200

    update

---
+++ DOM/ajaxForm.js
@@ -0,0 +1,76 @@
+import { dom } from "./DOM";
+
+let preloader, createAlertModal, addInputError;
+
+export const ajaxForm = (sets = {}) => {
+
+  preloader = sets.preloader;
+  createAlertModal = sets.createAlertModal;
+  addInputError = sets.addInputError;
+
+  dom.body.addEventListener('submit', e => {
+    let form = e.target;
+    if (form.classList.contains('ajax-form')) {
+      e.preventDefault();
+      init(form);
+    }
+  });
+}
+
+
+
+const init = form => {
+  let data = new FormData(form);
+  let url = form.getAttribute('action');
+  dom.ajax({ url, data, preloader: form, preloaderHTML: preloader, minTimeResponse: 300 })
+    .then(res => parseFormResponse(res, form));
+}
+
+
+
+const parseFormResponse = (res, form) => {
+  res = dom.jsonParse(res);
+  if (!res) return false;
+
+  if (res.redirect) {
+    window.location.href = res.redirect;
+  }
+
+  if (res.alert && createAlertModal && $) {
+    let modal = createAlertModal(res.alert);
+    $.fancybox.open(modal, { touch: false });
+  }
+
+  if (res.clearInputs) {
+    clearInputs(form);
+  }
+
+  if (res.inputError) {
+    addResponseInputsErrors(res.inputError, form);
+  }
+}
+
+
+
+
+
+const addResponseInputsErrors = (response, form) => {
+  for (let name in response) {
+    let inputs = dom.findAll(`[name=${name}]`, form);
+    if (!inputs || !inputs.length) continue;
+
+    inputs.forEach(input => addInputError(input, response[name]));
+  }
+}
+
+
+
+
+const clearInputs = form => {
+  let inputs = dom.findAll('input, textarea, select', form);
+  if (!inputs || !inputs.length) return;
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+}
---
+++ DOM/domFormValidate.js
@@ -0,0 +1,232 @@
+import { dom } from "./DOM";
+
+// if true will not validate form
+let DEBUG = false;
+
+let messages, addInputError, removeInputError;
+
+
+export const domFormValidate = (sets = {}) => {
+  let container = sets.container;
+
+  messages = sets.messages || {};
+  addInputError = sets.addInputError;
+  removeInputError = sets.removeInputError;
+  DEBUG = sets.DEBUG;
+
+  let forms = dom.findAll('.validate-form', container);
+  if (!forms || !forms.length) return;
+
+  forms.forEach(form => initValidate(form));
+}
+
+
+
+const initValidate = form => {
+  initRemoveError(form);
+
+  dom.onClick('[type=submit]', e => {
+    if (DEBUG) return;
+
+    let isValid = validate(form);
+    if (!isValid) {
+      e.preventDefault();
+    }
+  }, form);
+}
+
+
+
+
+const initRemoveError = form => {
+  if ($) {
+    $(form).on('change', e => {
+      let input = e.target;
+      removeInputError(input);
+    });
+    $('input[data-phone]').on('change', function () {
+      removeInputError(this);
+    });
+  } else {
+    form.addEventListner('change', e => {
+      let input = e.target;
+      removeInputError(input);
+    });
+  }
+}
+
+
+
+
+
+const validate = form => {
+  let inputs = dom.findAll('input, textarea, select', form);
+  if (!inputs || !inputs.length) return;
+
+  let errors = 0;
+
+  inputs.forEach(input => {
+    if (input.classList.contains('disabled')) return;
+
+    let res = validateInput(input);
+    if (!res) errors++;
+  });
+
+  return errors > 0 ? false : true;
+}
+
+
+
+
+const validateInput = input => {
+  if (hasInputError(input)) return false;
+
+  let res = true;
+
+  if (res && input.hasAttribute('data-required')) {
+    res = validateRequired(input);
+  }
+
+  if (res && input.type === 'email') {
+    res = validateEmail(input);
+  }
+
+  if (res && input.dataset.phone) {
+    res = validatePhone(input);
+  }
+
+  if (res && input.dataset.compare) {
+    res = validateCompare(input);
+  }
+
+  if (res && input.dataset.maxSize) {
+    res = validateMaxFileSize(input);
+  }
+
+  if (res && input.dataset.minLength) {
+    res = validateMinLength(input);
+  }
+
+  // if (res) removeInputError(input);
+  return res;
+}
+
+
+const validateMinLength = input => {
+  if (!input.value) return true;
+  let min = parseInt(input.dataset.minLength);
+  if (!min) return true;
+  if (input.value.length < min) {
+    addInputError(input, messages.minLength.replace(/\$1/, min));
+    return false;
+  }
+  return true;
+}
+
+
+
+
+
+const validateMaxFileSize = input => {
+  if (!input.files && !input.files[0]) return true;
+  let file = input.files[0];
+  if (!file) return true;
+  let size = file.size / 1000000;
+  let max = parseFloat(input.dataset.maxSize);
+  if (size > max) {
+    addInputError(input, messages.maxFileSize.replace(/\$1/, max));
+    return false;
+  }
+  return true;
+}
+
+
+
+const validateRequired = input => {
+
+  if (input.type === 'checkbox' || input.type === 'radio') {
+    return validateRequiredCheckbox(input);
+  }
+
+  if (!input.value) {
+    addInputError(input, messages.required);
+    return false;
+  }
+  return true;
+}
+
+
+
+
+const validateRequiredCheckbox = input => {
+  let name = input.name;
+  if (!name) return;
+  let allInputs = dom.findAll(`input[name=${name}]`);
+  if (!allInputs || !allInputs.length) return true;
+
+  let errCont = input.dataset.appendError;
+  if (errCont) {
+    errCont = dom.findFirst(errCont);
+  }
+
+  let isValid = allInputs.some(input => input.checked);
+
+  if (isValid) {
+    if (errCont) {
+      errCont.innerHTML = '';
+      dom.removeClass(errCont, 'has-error');
+    }
+    return true;
+  }
+
+  if (!errCont) return false;
+
+  dom.addClass(errCont, 'has-error');
+  errCont.innerHTML = messages.checkboxRequired;
+}
+
+
+
+const validateCompare = input => {
+  let compareInput = dom.findFirst(input.dataset.compare);
+  if (!compareInput) return;
+  let curVal = input.value;
+  let compareVal = compareInput.value;
+
+  if (curVal !== compareVal) {
+    addInputError(input, messages.invlidCompare);
+    addInputError(compareInput, messages.invlidCompare);
+    return false;
+  }
+
+  return true;
+}
+
+
+
+
+const validateEmail = input => {
+  if (!input.value) return true;
+  let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  let res = re.test(String(input.value).toLowerCase());
+  if (!res) {
+    addInputError(input, messages.invalidEmail);
+    return false;
+  }
+  return true;
+}
+
+
+
+
+const validatePhone = input => {
+  if (!input.value) return true;
+  return true;
+}
+
+
+
+
+const hasInputError = input => {
+  return input.classList.contains('has-error');
+}
